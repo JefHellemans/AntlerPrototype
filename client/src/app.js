7176ee1f99025ef78e7fc0e1d401bc7260be9587
Jef <hello@jefhellemans.com>
2015-12-09 16:38:15 +0100

    Canvas Drag N Drop scaling

--- client/src/app.js
+++ client/src/app.js
@@ -3,20 +3,69 @@ var ctx;
 var WIDTH;
 var HEIGHT;
 var trades = [];
+var scale = 1;
 var dragging = null;
+
+function cX() {
+    return (WIDTH / 2);
+}
+
+function cY() {
+    return (HEIGHT / 2);
+}
+
+function sfc(x, y) {
+    var a = (y - cY())/(x - cX());
+    var d = Math.sqrt(Math.pow((x - cX()), 2) + Math.pow((y - cY()), 2));
+    d = d * scale;
+    var angleY = (Math.atan(a) / Math.PI) * 180;
+    if(angleY < 0) {
+        angleY = -angleY;
+    }
+    var angleX = 90 - angleY;
+    var nX = d * Math.sin(((angleX / 180) * Math.PI));
+    var nY = d * Math.sin(((angleY / 180) * Math.PI));
+    if(x > cX()) {
+        nX = -nX;
+    }
+    if(y > cY()) {
+        nY = -nY;
+    }
+    return [cX() - nX, cY() - nY];
+}
+
+function sbfc(x, y) {
+    var a = (y - cY())/(x - cX());
+    var d = Math.sqrt(Math.pow((x - cX()), 2) + Math.pow((y - cY()), 2));
+    d = d * (1 / scale);
+    var angleY = (Math.atan(a) / Math.PI) * 180;
+    if(angleY < 0) {
+        angleY = -angleY;
+    }
+    var angleX = 90 - angleY;
+    var nX = Math.ceil(d * Math.sin(((angleX / 180) * Math.PI)));
+    var nY = Math.ceil(d * Math.sin(((angleY / 180) * Math.PI)));
+    if(x > cX()) {
+        nX = -nX;
+    }
+    if(y > cY()) {
+        nY = -nY;
+    }
+    return [cX() - nX, cY() - nY];
+}
 
 var rect = function(l,t,w,h) {
     ctx.beginPath();
     var x = l - (w/2);
     var y = t - (h/2);
-    ctx.rect(x,y,w,h);
+    ctx.rect(x, y, w * scale, h * scale);
     ctx.closePath();
     ctx.fill();
 };
 
 var cir = function(x, y, rad) {
     ctx.beginPath();
-    ctx.arc(x, y, rad, 0, 2 * Math.PI, false);
+    ctx.arc(x, y, rad * scale, 0, 2 * Math.PI, false);
     ctx.closePath();
     ctx.fill();
 };
@@ -42,7 +91,7 @@ function init() {
         for (var j = 0; j < 6; j++ ) {
             color += letters[Math.floor(Math.random() * 16)];
         }
-        trades.push(new Trade(i, x, y, rad, color));
+        trades.push(new Trade(i, x, y, x, y, rad, color));
     }
 
     return setInterval(draw, 10);
@@ -56,52 +105,64 @@ function draw() {
     }
 }
 
-function myMove(e){
+function myMove(e) {
     if (dragging !== null) {
-        dragging.x = e.pageX;
-        dragging.y = e.pageY;
+        dragging.scX = e.pageX;
+        dragging.scY = e.pageY;
     }
 }
 
-function myDown(e){
-    for(var i = trades.length -1 ; i >= 0; i--) {
+function myDown(e) {
+    for(var i = trades.length - 1; i >= 0; i--) {
         var trade = trades[i];
-        if((Math.pow((e.pageX - trade.x), 2) + Math.pow((e.pageY - trade.y), 2)) <= Math.pow(trade.rad, 2)) {
+        if((Math.pow((e.pageX - trade.scX), 2) + Math.pow((e.pageY - trade.scY), 2)) <= Math.pow((trade.rad * scale), 2)) {
             dragging = trade;
             trades.splice(i, 1);
             trades.push(trade);
             canvas.onmousemove = myMove;
-            canvas.touchmove = myMove;
             break;
         }
     }
 }
 
-function myUp(){
+function myUp() {
+    var p = sbfc(dragging.scX, dragging.scY);
+    dragging.x = p[0];
+    dragging.y = p[1];
     dragging = null;
     canvas.onmousemove = null;
-    canvas.touchmove = null;
 }
 
 init();
 canvas.onmousedown = myDown;
 canvas.onmouseup = myUp;
-canvas.touchstart = myDown;
-canvas.touchend = myDown;
 
-function Trade(text, x, y, rad, color) {
+document.getElementById("scaleCanvas").onmousemove = function() {
+    scale = this.value;
+    for(var i = trades.length - 1; i >= 0; i--) {
+        var trade = trades[i];
+        var p = sfc(trade.x, trade.y);
+        trade.scX = p[0];
+        trade.scY = p[1];
+    }
+};
+
+function Trade(text, x, y, scX, scY, rad, color) {
     this.text = text;
     this.x = x;
     this.y = y;
+    this.scX = scX;
+    this.scY = scY;
     this.rad = rad;
     this.color = color;
 }
 
 Trade.prototype.draw = function() {
     ctx.fillStyle = this.color;
-    cir(this.x, this.y, this.rad);
-    ctx.font = "30px Arial";
+    cir(this.scX, this.scY, this.rad);
+    var fontSize = 30 * scale;
+    ctx.font = fontSize + "px Arial";
     ctx.fillStyle = "#000000";
     ctx.textAlign = "center";
-    ctx.fillText(this.text, this.x, this.y);
+    ctx.fillText(this.text, this.scX, this.scY);
 };
