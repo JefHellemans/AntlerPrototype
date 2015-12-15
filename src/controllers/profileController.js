af42411026ebd6bf067b0b9bb5ca53155f031edd
marijnhosten <marijn.hosten@student.howest.be>
2015-12-15 12:23:34 +0100

    profile views

--- src/controllers/profileController.js
+++ src/controllers/profileController.js
@@ -7,6 +7,8 @@
 
         $scope.firstName = "Marijn";
         $scope.lastName = "Hosten";
+        $scope.passw1 = "";
+        $scope.passw2 = "";
     };
 
     angular.module("app").controller("profileController", [ "$scope", "$routeParams", profileController]);
